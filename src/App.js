2e4b325555da4c25d129f30f99eb5d4f649db2da
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-07-17 18:49:47 -0400

    added fps counter and living cell counter

--- src/App.js
+++ src/App.js
@@ -16,16 +16,58 @@ const directions = [
 function App() {
   const top = 60;
   const down = 50;
+  const initialSpeed = 5;
 
-  const gridRef = useRef(createClearGrid(top, down));
+  const countLiveNeighbors = useCallback((currentGrid, row, col) => {
+    let neighborCount = 0;
 
-  function createClearGrid(width, height) {
+    for (let i = 0; i < directions.length; i++) {
+      const [dx, dy] = directions[i];
+      let newRow = row + dx;
+      let newCol = col + dy;
+
+      // Wrap around if the neighbor is out of bounds
+      if (newRow < 0) newRow = down - 1;
+      if (newRow >= down) newRow = 0;
+      if (newCol < 0) newCol = top - 1;
+      if (newCol >= top) newCol = 0;
+
+      if (currentGrid[newRow][newCol] === 1) {
+        neighborCount++;
+      }
+    }
+
+    return neighborCount;
+  }, [down, top]);
+
+  const countLivingCells = useCallback((currentGrid) => {
+    let count = 0;
+
+    for (let row = 0; row < down; row++) {
+      for (let col = 0; col < top; col++) {
+        if (currentGrid[row][col] === 1) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }, [down, top]);
+
+  const [speed, setSpeed] = useState(initialSpeed);
+  const [grid, setGrid] = useState(() => createGrid(top, down));
+  const [gameStart, setGameStart] = useState(false);
+
+  const fpsRef = useRef(0);
+  const livingCellsRef = useRef(0);
+
+  function createGrid(width, height) {
     const grid = [];
 
     for (let row = 0; row < height; row++) {
       const rowArray = [];
       for (let col = 0; col < width; col++) {
-        rowArray.push(0);
+        rowArray.push(0); // Change this line to customize the initial value of each cell
       }
       grid.push(rowArray);
     }
@@ -33,16 +75,11 @@ function App() {
     return grid;
   }
 
-  const [gameStart, setGameStart] = useState(false);
-  const [speed, setSpeed] = useState(1); // Initial value set to 1
-
   const handleBlockClick = (rowIndex, colIndex) => {
     if (!gameStart) {
-      const newGrid = produce(gridRef.current, (gridCopy) => {
-        gridCopy[rowIndex][colIndex] = 1 - gridCopy[rowIndex][colIndex];
+      const newGrid = produce(grid, (draftGrid) => {
+        draftGrid[rowIndex][colIndex] = 1 - draftGrid[rowIndex][colIndex];
       });
-
-      gridRef.current = newGrid;
       setGrid(newGrid);
     }
   };
@@ -52,114 +89,69 @@ function App() {
   };
 
   const handleClearGrid = () => {
-    setGameStart(false);
-    const newGrid = createClearGrid(top, down);
-    gridRef.current = newGrid;
-    setGrid(newGrid);
+    if (!gameStart) {
+      const newGrid = createGrid(top, down);
+      setGrid(newGrid);
+      livingCellsRef.current = 0;
+    }
   };
 
   const handleRandomizeGrid = () => {
     if (!gameStart) {
-      const newGrid = produce(gridRef.current, (gridCopy) => {
+      const newGrid = produce(grid, (draftGrid) => {
         for (let row = 0; row < down; row++) {
           for (let col = 0; col < top; col++) {
-            gridCopy[row][col] = Math.random() < 0.1 ? 1 : 0;
+            draftGrid[row][col] = Math.random() > 0.7 ? 1 : 0;
           }
         }
       });
-
-      gridRef.current = newGrid;
       setGrid(newGrid);
+      livingCellsRef.current = countLivingCells(newGrid);
     }
   };
 
-  const computeNextGeneration = useCallback(
-    (currentGrid) => {
-      return produce(currentGrid, (gridCopy) => {
-        for (let row = 0; row < down; row++) {
-          for (let col = 0; col < top; col++) {
-            const currCell = currentGrid[row][col];
-            const neighbors = countLiveNeighbors(currentGrid, row, col);
-
-            if (currCell === 1 && (neighbors < 2 || neighbors > 3)) {
-              gridCopy[row][col] = 0;
-            } else if (currCell === 0 && neighbors === 3) {
-              gridCopy[row][col] = 1;
-            }
+  const computeNextGeneration = useCallback((currentGrid) => {
+    const newGrid = produce(currentGrid, (draftGrid) => {
+      for (let row = 0; row < down; row++) {
+        for (let col = 0; col < top; col++) {
+          let currCell = currentGrid[row][col];
+          const neighbors = countLiveNeighbors(currentGrid, row, col);
+          let newCell = 0;
+          if (currCell === 1 && (neighbors === 2 || neighbors === 3)) {
+            newCell = 1;
+          } else if (currCell === 0 && neighbors === 3) {
+            newCell = 1;
           }
-        }
-      });
-    },
-    [down, top]
-  );
-
-  const countLiveNeighbors = useCallback(
-    (currentGrid, row, col) => {
-      let neighborCount = 0;
-
-      for (let i = 0; i < directions.length; i++) {
-        const [dx, dy] = directions[i];
-        let newRow = row + dx;
-        let newCol = col + dy;
-
-        // Wrap around if the neighbor is out of bounds
-        if (newRow < 0) newRow = down - 1;
-        if (newRow >= down) newRow = 0;
-        if (newCol < 0) newCol = top - 1;
-        if (newCol >= top) newCol = 0;
-
-        if (currentGrid[newRow][newCol] === 1) {
-          neighborCount++;
+          draftGrid[row][col] = newCell;
         }
       }
+    });
 
-      return neighborCount;
-    },
-    [down, top]
-  );
+    return newGrid;
+  }, [countLiveNeighbors, down, top]);
 
-  const [grid, setGrid] = useState(gridRef.current);
+  const updateFPS = useCallback(() => {
+    fpsRef.current = Math.round(1000 / (speed * 40));
+  }, [speed]);
 
-  useEffect(() => {
-    gridRef.current = grid;
-  }, [grid]);
-
-  const requestRef = useRef();
-  const previousTimeRef = useRef();
-
-  const updateGrid = useCallback(
-    (currentTime) => {
-      if (!previousTimeRef.current) {
-        previousTimeRef.current = currentTime;
-      }
-
-      const deltaTime = currentTime - previousTimeRef.current;
-      const delay = 1000 / (speed * 2); // Adjust the multiplier for a slower scale
-
-      if (deltaTime > delay) {
-        setGrid((prevGrid) => {
-          const newGrid = computeNextGeneration(prevGrid);
-          return newGrid;
-        });
-
-        previousTimeRef.current = currentTime;
-      }
-
-      requestRef.current = requestAnimationFrame(updateGrid);
-    },
-    [computeNextGeneration, speed]
-  );
+  const updateLivingCells = useCallback(() => {
+    livingCellsRef.current = countLivingCells(grid);
+  }, [countLivingCells, grid]);
 
   useEffect(() => {
-    if (gameStart) {
-      requestRef.current = requestAnimationFrame(updateGrid);
-    } else {
-      cancelAnimationFrame(requestRef.current);
-      previousTimeRef.current = null;
-    }
+    const interval = setInterval(() => {
+      if (gameStart) {
+        setGrid((prevGrid) => computeNextGeneration(prevGrid));
+      }
+    }, speed * 40);
 
-    return () => cancelAnimationFrame(requestRef.current);
-  }, [gameStart, updateGrid]);
+    return () => clearInterval(interval);
+  }, [computeNextGeneration, gameStart, speed]);
+
+  useEffect(() => {
+    updateFPS();
+    updateLivingCells();
+  }, [updateFPS, updateLivingCells]);
 
   return (
     <div className="body">
@@ -187,12 +179,14 @@ function App() {
               type="range"
               className="computationSpeedSlider"
               min={1}
-              max={20}
-              value={speed}
-              onChange={(e) => {
-                setSpeed(parseInt(e.target.value, 10));
-              }}
+              max={10}
+              value={11 - speed}
+              onChange={(e) => setSpeed(11 - parseInt(e.target.value))}
             />
+          </div>
+          <div className="info-wrapper">
+            <div className="fps-counter">FPS: {fpsRef.current}</div>
+            <div className="living-cells-counter">Living Cells: {livingCellsRef.current}</div>
           </div>
           <div className="buttons-wrapper">
             <button className="ClearGrid" onClick={handleClearGrid}>
@@ -207,7 +201,6 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="options"></div>
     </div>
   );
 }
