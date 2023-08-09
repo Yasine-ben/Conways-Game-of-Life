a25279bb05675f812a3e6ce0f758ad847f656b6b
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-08-09 11:23:02 -0400

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,127 +1,88 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 import produce from 'immer';
-// import video from './video.webm';
 
-const directions = [
-  [-1, -1], // top left
-  [-1, 0], // top
-  [-1, 1], // top right
-  [0, -1], // left
-  [0, 1], // right
-  [1, -1], // bottom left
-  [1, 0], // bottom
-  [1, 1], // bottom right
+
+const DIRECTIONS = [
+  [-1, -1], [-1, 0], [-1, 1],
+  [0, -1], [0, 1],
+  [1, -1], [1, 0], [1, 1],
 ];
 
-const gliderPattern = [
+const GLIDER_PATTERN = [
   [0, 1, 0],
   [0, 0, 1],
   [1, 1, 1],
 ];
 
-// const singleBlockPattern = [
-//   [1]
-// ]
+const TOP = 60;
+const DOWN = 50;
+const INITIAL_SPEED = 5;
 
-// const LWSS = [
-
-// ]
-
-// const MWSS = [
-
-// ]
-
-// const HWSS = [
-
-// ]
-
-// add total cycle counter
-// add diff options for patterns
-// reorder buttons
-// ethan stays
-// make a splash screen 
-
+const createGrid = (width, height) => {
+  const grid = [];
+  for (let row = 0; row < height; row++) {
+    const rowArray = [];
+    for (let col = 0; col < width; col++) {
+      rowArray.push(0);
+    }
+    grid.push(rowArray);
+  }
+  return grid;
+};
 
 const App = () => {
-  const top = 60;
-  const down = 50;
-  const initialSpeed = 5;
-
   const [selectedOption, setSelectedOption] = useState('Block');
-  const [singleBlockPreview, setSingleBlockPreview] = useState(null);
+  const [speed, setSpeed] = useState(INITIAL_SPEED);
+  const [grid, setGrid] = useState(() => createGrid(TOP, DOWN));
+  const [gameStart, setGameStart] = useState(false);
+  const [gliderPreviewRow, setGliderPreviewRow] = useState(null);
+  const [gliderPreviewCol, setGliderPreviewCol] = useState(null);
+  const [singleBlockPreview, setSingleBlockPreview] = useState({ rowIndex: null, colIndex: null });
 
+  const fpsRef = useRef(0);
+  const livingCellsRef = useRef(0);
 
   const countLiveNeighbors = useCallback((currentGrid, row, col) => {
     let neighborCount = 0;
-
-    for (let i = 0; i < directions.length; i++) {
-      const [dx, dy] = directions[i];
+    for (let i = 0; i < DIRECTIONS.length; i++) {
+      const [dx, dy] = DIRECTIONS[i];
       let newRow = row + dx;
       let newCol = col + dy;
-
-      // Wrap around if the neighbor is out of bounds
-      if (newRow < 0) newRow = down - 1;
-      if (newRow >= down) newRow = 0;
-      if (newCol < 0) newCol = top - 1;
-      if (newCol >= top) newCol = 0;
-
+      if (newRow < 0) newRow = DOWN - 1;
+      if (newRow >= DOWN) newRow = 0;
+      if (newCol < 0) newCol = TOP - 1;
+      if (newCol >= TOP) newCol = 0;
       if (currentGrid[newRow][newCol] === 1) {
         neighborCount++;
       }
     }
-
     return neighborCount;
-  }, [down, top]);
+  }, [DOWN, TOP]);
 
   const countLivingCells = useCallback((currentGrid) => {
     let count = 0;
-
-    for (let row = 0; row < down; row++) {
-      for (let col = 0; col < top; col++) {
+    for (let row = 0; row < DOWN; row++) {
+      for (let col = 0; col < TOP; col++) {
         if (currentGrid[row][col] === 1) {
           count++;
         }
       }
     }
-
     return count;
-  }, [down, top]);
-
-  const [speed, setSpeed] = useState(initialSpeed);
-  const [grid, setGrid] = useState(() => createGrid(top, down));
-  const [gameStart, setGameStart] = useState(false);
-  const [gliderPreviewRow, setGliderPreviewRow] = useState(null);
-  const [gliderPreviewCol, setGliderPreviewCol] = useState(null);
-
-  const fpsRef = useRef(0);
-  const livingCellsRef = useRef(0);
-
-  function createGrid(width, height) {
-    const grid = [];
-
-    for (let row = 0; row < height; row++) {
-      const rowArray = [];
-      for (let col = 0; col < width; col++) {
-        rowArray.push(0); // Change this line to customize the initial value of each cell
-      }
-      grid.push(rowArray);
-    }
-
-    return grid;
-  }
+  }, [DOWN, TOP]);
 
   const handleBlockClick = (rowIndex, colIndex) => {
     if (!gameStart) {
       const newGrid = produce(grid, (draftGrid) => {
         if (selectedOption === 'Glider') {
-          for (let i = 0; i < gliderPattern.length; i++) {
-            for (let j = 0; j < gliderPattern[i].length; j++) {
-              draftGrid[rowIndex + i][colIndex + j] = selectedOption === 'Glider' ? gliderPattern[i][j] : 1 - draftGrid[rowIndex + i][colIndex + j];
+          for (let i = 0; i < GLIDER_PATTERN.length; i++) {
+            for (let j = 0; j < GLIDER_PATTERN[i].length; j++) {
+              draftGrid[rowIndex + i][colIndex + j] = selectedOption === 'Glider' ? GLIDER_PATTERN[i][j] : 1 - draftGrid[rowIndex + i][colIndex + j];
             }
           }
         } else if (selectedOption === 'Block') {
-          draftGrid[rowIndex][colIndex] = 1
+          draftGrid[rowIndex][colIndex] = 1;
         }
       });
       setGrid(newGrid);
@@ -132,13 +93,12 @@ const App = () => {
     if (!gameStart) {
       if (selectedOption === 'Block') {
         setSingleBlockPreview({ row: rowIndex, col: colIndex });
-        setGliderPreviewRow(null); // Clear glider preview
+        setGliderPreviewRow(null);
         setGliderPreviewCol(null);
       } else if (selectedOption === 'Glider') {
         setGliderPreviewRow(rowIndex);
         setGliderPreviewCol(colIndex);
       }
-
     }
   };
 
@@ -154,8 +114,8 @@ const App = () => {
   };
 
   const handleClearGrid = () => {
-    setGameStart(false)
-    const newGrid = createGrid(top, down);
+    setGameStart(false);
+    const newGrid = createGrid(TOP, DOWN);
     setGrid(newGrid);
     livingCellsRef.current = 0;
   };
@@ -163,8 +123,8 @@ const App = () => {
   const handleRandomizeGrid = () => {
     if (!gameStart) {
       const newGrid = produce(grid, (draftGrid) => {
-        for (let row = 0; row < down; row++) {
-          for (let col = 0; col < top; col++) {
+        for (let row = 0; row < DOWN; row++) {
+          for (let col = 0; col < TOP; col++) {
             draftGrid[row][col] = Math.random() > 0.7 ? 1 : 0;
           }
         }
@@ -176,8 +136,8 @@ const App = () => {
 
   const computeNextGeneration = useCallback((currentGrid) => {
     const newGrid = produce(currentGrid, (draftGrid) => {
-      for (let row = 0; row < down; row++) {
-        for (let col = 0; col < top; col++) {
+      for (let row = 0; row < DOWN; row++) {
+        for (let col = 0; col < TOP; col++) {
           let currCell = currentGrid[row][col];
           const neighbors = countLiveNeighbors(currentGrid, row, col);
           let newCell = 0;
@@ -190,9 +150,8 @@ const App = () => {
         }
       }
     });
-
     return newGrid;
-  }, [countLiveNeighbors, down, top]);
+  }, [countLiveNeighbors, DOWN, TOP]);
 
   const updateFPS = useCallback(() => {
     fpsRef.current = Math.round(speed);
@@ -231,19 +190,17 @@ const App = () => {
                   key={colIndex}
                   className="block"
                   style={{
-
                     backgroundColor: col === 1 ? 'white' : 'black',
-                    opacity:
-                      (gliderPreviewRow !== null &&
-                        rowIndex >= gliderPreviewRow &&
-                        rowIndex < gliderPreviewRow + gliderPattern.length &&
-                        colIndex >= gliderPreviewCol &&
-                        colIndex < gliderPreviewCol + gliderPattern[0].length) ||
-                        (singleBlockPreview !== null &&
-                          rowIndex === singleBlockPreview.rowIndex &&
-                          colIndex === singleBlockPreview.colIndex)
-                        ? '0.5'
-                        : '1',
+                    opacity: (gliderPreviewRow !== null &&
+                      rowIndex >= gliderPreviewRow &&
+                      rowIndex < gliderPreviewRow + GLIDER_PATTERN.length &&
+                      colIndex >= gliderPreviewCol &&
+                      colIndex < gliderPreviewCol + GLIDER_PATTERN[0].length) ||
+                      (singleBlockPreview.rowIndex !== null &&
+                        rowIndex === singleBlockPreview.rowIndex &&
+                        colIndex === singleBlockPreview.colIndex)
+                      ? '0.5'
+                      : '1',
                   }}
                   onClick={() => handleBlockClick(rowIndex, colIndex)}
                   onMouseEnter={() => handleBlockHover(rowIndex, colIndex)}
@@ -254,38 +211,19 @@ const App = () => {
           ))}
         </div>
         <div className="controller-wrapper">
-          <div className="slider-wrapper">
-            Speed
-            <input
-              type="range"
-              className="computationSpeedSlider"
-              min={1}
-              max={25}
-              value={speed}
-              onChange={(e) => setSpeed(parseInt(e.target.value))}
-            />
-          </div>
-          <div className="info-wrapper">
-            <div className="fps-counter">FPS: {fpsRef.current}</div>
-            <div className="living-cells-counter">Living Cells: {livingCellsRef.current}</div>
-          </div>
           <div className="buttons-wrapper">
-            <button className="Button" onClick={handleGameStart}>
-              {gameStart ? 'Stop' : 'Start'}
-            </button>
-            <button className="Button" onClick={handleClearGrid}>
-              Clear
-            </button>
-            <button className="Button" onClick={handleRandomizeGrid}>
-              Randomize
-            </button>
-            <button className='Button' onClick={() => setSelectedOption(selectedOption === 'Block' ? 'Glider' : 'Block')}>
-              {selectedOption === 'Block' ? 'Glider Selector' : 'Block Selector'}
-            </button>
+            <button className="Button GameStartButton" onClick={handleGameStart}>{gameStart ? 'Stop' : 'Start'}</button>
+            <button className="Button ClearGrid" onClick={handleClearGrid}>Clear</button>
+            <button className="Button RandomizeGrid" onClick={handleRandomizeGrid}>Randomize</button>
+          </div>
+          <div className='counters-wrapper'>
+            <label className="living-cells-counter">Living Cells: {livingCellsRef.current}</label>
+          </div>
+          <div className="slider-wrapper">            
+            <label className="fps-counter">FPS: {fpsRef.current}</label>
+            <input className="computationSpeedSlider" type="range" min="1" max="60" value={speed} onChange={(e) => setSpeed(e.target.value)} />
           </div>
         </div>
-        {/* <video id="transparentVideo" autoPlay loop src={video} muted>
-        </video> */}
       </div>
     </div>
   );
